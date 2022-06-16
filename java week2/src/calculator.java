ee93fc5d195acdd42e4215dbd961bff2b88fb8d1
Aravind P <107612670+ARAVIND-PV1@users.noreply.github.com>
2022-06-16 10:30:06 +0100

    Add files via upload

---
+++ java week2/src/calculator.java
@@ -0,0 +1,15 @@
+
+public class calculator {
+	public static void main(String[] args) {
+	      int[][] multidimensionalArray = { {1,6,9,6,3},{2,11,7,4,8},{9,0,1,8,3}
+	      };
+	      for(int i = 0 ; i < 6 ; i++)
+	      {
+	          for(int j = 0 ; j < 4; j++)
+	          {
+	             System.out.print(multidimensionalArray[i][j] + " ");
+	          }
+	          System.out.println();
+	       }
+	    }
+	 }    
---
+++ java week2/src/jink.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class jink {
+
+	public static void main(String[] args) {
+		Scanner f=new Scanner(System.in);
+int i= 10;
+		while(i<20) {
+	System.out.println("i");
+	i++;
+	System.out.println("end");
+}
+		}
+
+	
+}
